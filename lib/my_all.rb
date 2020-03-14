62329eed0af44b87160221b64a7879360e922cc3
Jaquan Piper <jaquan.piper@gmail.com>
2020-03-14 20:37:16 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,22 @@
-require 'pry'
+def my_all?(collection)
+  i = 0
+  while i < collection.length
+    i = i + 1   
+  end
+end
 
 def my_all?(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
+  end
+end
 
+def my_all?([1,2,3])
+  i = 0
+  while i < 3
+    yield(1)
+    i = 0 + 1
+  end
 end
